17a426020233c25cb56efa4241c892a902be6686
nile <nile.black@gmail.com>
2012-03-09 12:38:39 +0800

    修改hash值长度

--- app/utils/HashUtil.java
+++ app/utils/HashUtil.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang.RandomStringUtils;
 
 public class HashUtil {
 	public static String hash() {
-		return RandomStringUtils.random(8, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		return RandomStringUtils.random(16, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	}
 	public static String deleteHash(String hash) {
 		return "_"+hash+"_";
